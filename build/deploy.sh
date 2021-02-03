967ffb6ea5490f914aa15fd1e2d5764e9a73e424
adlong <3140618@163.com>
2021-02-03 18:10:43 +0800

    update

--- build/deploy.sh
+++ build/deploy.sh
@@ -8,10 +8,10 @@ build() {
     for lang in ${langs[@]}
       do
       cp -f ../md/$product/v1/$lang/index.html.md source/index.html.md
-      bundle exec middleman build --clean --build-dir ../html/docs/$product/v1/$lang/
+      bundle exec middleman build --clean --build-dir ../docs/$product/v1/$lang/
       done
     done
-  cp -rf ../html/docs/usdt_swap/v1/cn/* ../html/docs/
+  cp -rf ../docs/usdt_swap/v1/cn/* ../docs/
 }
 
 build
